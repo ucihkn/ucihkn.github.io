ba32f0bf64bced9757d24123300c2cb7bf75682f
Neet <neetp1@uci.edu>
2025-12-22 15:08:33 +0530

    Update PastEvents.jsx

--- Frontend/src/pages/PastEvents.jsx
+++ Frontend/src/pages/PastEvents.jsx
@@ -8,7 +8,7 @@ function PastEvents() {
             <>
                 <h1 className="display-1 mb-1">Past Events</h1>
                 <p className="lead mb-1">Explore our previous events and activities!</p>
-                <Button className="mb-5" variant="dark" onClick={() => window.location.href = "/past-events"}>Looking for our Past Events? Click here!</Button>
+                <Button className="mb-5" variant="dark" onClick={() => window.location.href = "/events"}>Looking for our Current Events? Click here!</Button>
                 <PastEventsSection showTitle={false} showViewAllButton={false} maxEvents={100} />
             </>
         } />
